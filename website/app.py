8ef41583a1ddc20fb4c976a387f080ad7cad11fc
Joe Eacott <eacott@Joes-MacBook-Pro.local>
2020-10-11 17:13:47 -0600

    add pytest to project

--- website/app.py
+++ website/app.py
@@ -4,11 +4,14 @@ This file initializes your application and brings together all of the various co
 from flask import Flask
 from website import views, models, forms
 
-app = Flask(__name__)
-app.register_blueprint(views.page)
-app.config.from_object('config')
+def create_app():
+    app_instance = Flask(__name__)
+    app_instance.register_blueprint(views.page)
+    app_instance.config.from_object('config')
+    return app_instance
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.config["LOG"].info('Starting environment')
     app.run(host='127.0.0.1')
